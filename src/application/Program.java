4d22e2503c6bf0588a5a7bd3c12e64baa6a9cb00
Riquelme Oliveira <riquuelmelive@gmail.com>
2023-02-08 10:35:35 -0300

    Primeira classe: Position

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardgame.position;
+
+public class Program {
+    public static void main(String[] args) {
+        
+            position pos = new position(3, 5);
+            System.out.println(pos);
+    }
+}
