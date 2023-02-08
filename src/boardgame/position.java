00fd7bbde65542d69930babd8092ea12d15ee15f
Riquelme Oliveira <riquuelmelive@gmail.com>
2023-02-08 11:56:32 -0300

    Iniciando a implementar o tabuleiro e peças

--- src/boardgame/position.java
+++ src/boardgame/position.java
@@ -1,13 +1,13 @@
 package boardgame;
 
-public class position {
+public class Position {
     
     private int row;
     private int column;
 
-    public position(){ 
+    public Position(){ 
     }
-    public position(int row, int column) {
+    public Position(int row, int column) {
         this.row = row;
         this.column = column;
     }
